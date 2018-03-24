f18743c412131147181d0eb01ac749942a1291ae
pestovpvl <pestovpvl@mail.ru>
2018-03-23 22:28:28 -0400

    Add Client Entity

--- app.rb
+++ app.rb
@@ -1,5 +1,15 @@
 require 'rubygems'
 require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:barbershop.db"
+
+class Client < ActiveRecord::Base
+  
+end
+
+
 
 configure do
   enable :sessions
